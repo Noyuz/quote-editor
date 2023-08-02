e23db18e9f3f83402536ebb699b687b8cdccbceb
Pierre-Elie ABERGEL <peabergel@PE-Macbook.local>
2023-08-02 16:50:52 +0200

    Beginning app

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,6 @@
+require "test_helper"
+require "selenium/webdriver"
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+end
